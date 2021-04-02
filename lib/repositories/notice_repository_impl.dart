4f663eb0f3a09cb84840f7a3c91ab6ae3aed9dc1
Funncy <gywns41@naver.com>
2021-04-02 14:35:02 +0900

    Implement Error Flow

--- lib/repositories/notice_repository_impl.dart
+++ lib/repositories/notice_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dream/error/error_model.dart';
 import 'package:dream/models/notice.dart';
 import 'package:dream/repositories/notice_repository.dart';
 import 'package:flutter/foundation.dart';
@@ -13,29 +14,33 @@ class NoticeRepositoryImpl extends NoticeRepository {
   @override
   Future<List<NoticeModel>> getNotices() async {
     List<NoticeModel> notices = [];
-    //notice 가져오기
-    var noticeCollection = await _firebaseFirestore.collection('notice').get();
-    for (var notice in noticeCollection.docs) {
-      notices.add(NoticeModel.fromFirestroe(notice));
 
-      // Comment 가져오기
-      List<NoticeCommentModel> comments = [];
-      var commentCollection =
-          await notice.reference.collection('comment').get();
-      for (var comment in commentCollection.docs) {
-        comments.add(NoticeCommentModel.fromFirestroe(comment));
-        // Reply 가져오기
-        List<NoticeCommentReplyModel> replys = [];
-        var replyCollection = await comment.reference.collection('reply').get();
-        for (var reply in replyCollection.docs) {
-          replys.add(NoticeCommentReplyModel.fromFirestroe(reply));
+    try {
+      var noticeCollection =
+          await _firebaseFirestore.collection('notice').get();
+      for (var notice in noticeCollection.docs) {
+        notices.add(NoticeModel.fromFirestroe(notice));
+
+        // Comment 가져오기
+        List<NoticeCommentModel> comments = [];
+        var commentCollection =
+            await notice.reference.collection('comment').get();
+        for (var comment in commentCollection.docs) {
+          comments.add(NoticeCommentModel.fromFirestroe(comment));
+          // Reply 가져오기
+          List<NoticeCommentReplyModel> replys = [];
+          var replyCollection =
+              await comment.reference.collection('reply').get();
+          for (var reply in replyCollection.docs) {
+            replys.add(NoticeCommentReplyModel.fromFirestroe(reply));
+          }
+          comments.last.replys = replys?.cast<NoticeCommentReplyModel>();
         }
-        comments.last.replys = replys?.cast<NoticeCommentReplyModel>();
+        notices.last.comments = comments?.cast<NoticeCommentModel>();
       }
-      notices.last.comments = comments?.cast<NoticeCommentModel>();
+    } catch (e) {
+      throw ErrorModel(message: '파이어베이스 에러');
     }
-    //inner Collection
-
     return notices;
   }
 }
