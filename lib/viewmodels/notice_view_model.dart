6534c08ba5453eff49f0087cc2768e48680773a6
Funncy <gywns41@naver.com>
2021-04-02 14:35:42 +0900

    Impelement notice_view_model test impl

--- lib/viewmodels/notice_view_model.dart
+++ lib/viewmodels/notice_view_model.dart
@@ -2,11 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dream/models/notice.dart';
 import 'package:dream/repositories/notice_repository.dart';
 import 'package:dream/repositories/notice_repository_impl.dart';
+import 'package:dream/viewmodels/common/screen_status.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-class NoticeViewModel extends GetxController {
+class NoticeViewModel extends GetxController with ScreenStatus {
   NoticeRepository _noticeRepository;
   RxList<NoticeModel> notices = <NoticeModel>[].obs;
 
@@ -15,7 +16,29 @@ class NoticeViewModel extends GetxController {
   }
 
   void getNotices() async {
-    var data = await _noticeRepository.getNotices();
-    if (data != null) notices.addAll(data);
+    List<NoticeModel> result = [];
+    //로딩 상태
+    setScreenStatus(Status.loading);
+    try {
+      result = await _noticeRepository.getNotices();
+    } catch (e) {
+      print(e);
+      //데이터 로드 실패
+      //세부적인 에러 내용은 ErrorModel에 담겨 있음
+      setScreenStatus(Status.error);
+      return;
+    }
+
+    if (result != null) {
+      //초기화 후 데이터 삽입
+      notices.clear();
+      notices.addAll(result);
+    }
+
+    if (result.length == 0) {
+      setScreenStatus(Status.empty);
+    } else {
+      setScreenStatus(Status.loaded);
+    }
   }
 }
