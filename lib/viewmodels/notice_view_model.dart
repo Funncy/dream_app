bd9c2a794de69a0cad022b3df4985d930a9e41ad
Funncy <gywns41@naver.com>
2021-04-24 20:39:53 +0900

    Refactory remove mixin class and write either in viewModel

--- lib/viewmodels/notice_view_model.dart
+++ lib/viewmodels/notice_view_model.dart
@@ -8,7 +8,7 @@ import 'package:flutter/foundation.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-class NoticeViewModel extends GetxController with GetDataWithScreen {
+class NoticeViewModel extends GetxController {
   NoticeRepository _noticeRepository;
   //obs는 observer로 Rx로 데이터 관리되게 됩니다.
   //화면 상태가 아니라 데이터의 상태를 관리하자.
@@ -16,6 +16,7 @@ class NoticeViewModel extends GetxController with GetDataWithScreen {
   Rx<Status> noticeStatus = Status.initial.obs;
   RxList<NoticeCommentModel> commentList = <NoticeCommentModel>[].obs;
   Rx<Status> commentStatus = Status.initial.obs;
+  //답글은 comment내부에 존재하지만 상태는 따로 관리
   Rx<Status> replyStatus = Status.initial.obs;
   //RxList<NoticeModel> not sub type List<NoticeModel>
 
@@ -27,18 +28,40 @@ class NoticeViewModel extends GetxController with GetDataWithScreen {
     //데이터 상태와 데이터를 가져오는 함수를 전달
     //추가로 리스트 형태인지를 전달
     // 리스트 형태인경우 데이터의 길이에 따라 Empty위젯을 보여줘야 함.
-    List<NoticeModel> result = await getDataWithScreenStatus(
-        dataStatus: noticeStatus,
-        getData: _noticeRepository.getNoticeList,
-        isInitial: true,
-        isList: true);
+    noticeStatus.value = Status.loading;
+    Either<ErrorModel, List<NoticeModel>> either =
+        await _noticeRepository.getNoticeList();
+    var result = either.fold((l) {
+      noticeStatus.value = Status.error;
+    }, (r) => r);
 
-    if (result != null) {
-      noticeList.clear();
-      noticeList.addAll(result);
+    //에러인 경우 종료
+    if (either.isLeft()) return;
 
-      checkEmptyWithScreenStatus(dataStatus: noticeStatus, result: result);
-    }
+    //Right이면 List로 반환됨.
+    noticeList.clear();
+    noticeList.addAll(result);
+    if (noticeList.length > 0)
+      noticeStatus.value = Status.loaded;
+    else
+      noticeStatus.value = Status.empty;
+  }
+
+  void getCommentList({@required String nid}) async {
+    Either<ErrorModel, List<NoticeCommentModel>> either =
+        await _noticeRepository.getCommentList(nid);
+    var result =
+        either.fold((l) => commentStatus.value = Status.error, (r) => r);
+
+    if (either.isLeft()) return;
+
+    commentList.clear();
+    commentList.addAll(result);
+
+    if (commentList.length > 0)
+      commentStatus.value = Status.loaded;
+    else
+      commentStatus.value = Status.empty;
   }
 
   void writeComment(
@@ -47,30 +70,30 @@ class NoticeViewModel extends GetxController with GetDataWithScreen {
       @required String content}) async {
     //update 중
     commentStatus.value = Status.updating;
-    Either<ErrorModel, void> updated = await _noticeRepository.writeComment(
+    Either<ErrorModel, void> either = await _noticeRepository.writeComment(
         nid: nid, uid: uid, content: content);
-    updated.fold((l) {
+    either.fold((l) {
       commentStatus.value = Status.error;
-      return;
     }, (r) {});
 
     //에러인 경우 아래 진행 안함
-    if (updated.isLeft()) return;
+    if (either.isLeft()) return;
 
     //정상적으로 서버 통신 완료
     //댓글 다시 읽어 오기
-    List<NoticeCommentModel> result = await getDataWithScreenStatus(
-        dataStatus: commentStatus,
-        getData: () async => await _noticeRepository.getCommentList(nid),
-        isInitial: false,
-        isList: true);
+    Either<ErrorModel, List<NoticeCommentModel>> either2 =
+        await _noticeRepository.getCommentList(nid);
+    var result =
+        either2.fold((l) => commentStatus.value = Status.error, (r) => r);
 
-    if (result != null) {
-      commentList.clear();
-      commentList.addAll(result);
+    if (either2.isLeft()) return;
 
-      checkEmptyWithScreenStatus(dataStatus: commentStatus, result: result);
-    }
+    commentList.clear();
+    commentList.addAll(result);
+    if (commentList.length > 0)
+      commentStatus.value = Status.loaded;
+    else
+      commentStatus.value = Status.empty;
   }
 
   Future<void> writeReply(
@@ -80,10 +103,10 @@ class NoticeViewModel extends GetxController with GetDataWithScreen {
       @required String content}) async {
     replyStatus.value = Status.updating;
     commentStatus.value = Status.updating;
-    Either<ErrorModel, void> updated = await _noticeRepository.writeReply(
+    Either<ErrorModel, void> either = await _noticeRepository.writeReply(
         did: did, uid: uid, content: content);
 
-    updated.fold((l) {
+    either.fold((l) {
       replyStatus.value = Status.error;
       //댓글 화면은 일단 그대로
       commentStatus.value = Status.loaded;
@@ -91,7 +114,7 @@ class NoticeViewModel extends GetxController with GetDataWithScreen {
     }, (r) {});
 
     //에러인 경우 아래 진행 안함
-    if (updated.isLeft()) return;
+    if (either.isLeft()) return;
 
     NoticeCommentModel comment =
         commentList.where((comment) => comment.did == did).first;
@@ -100,33 +123,14 @@ class NoticeViewModel extends GetxController with GetDataWithScreen {
 
     //정상적으로 서버 통신 완료
     //답글 다시 읽어 오기
-    List<NoticeCommentReplyModel> result = await getDataWithScreenStatus(
-        dataStatus: commentStatus,
-        getData: () async => await _noticeRepository.getReplyList(comment.did),
-        isInitial: false,
-        isList: false);
+    var either2 = await _noticeRepository.getReplyList(comment.did);
+    var result =
+        either2.fold((l) => commentStatus.value = Status.error, (r) => r);
 
     comment.replys.clear();
     comment.replys.addAll(result);
 
     replyStatus.value = Status.loaded;
     commentStatus.value = Status.loaded;
-  }
-
-  void getCommentList({@required String nid}) async {
-    List<NoticeCommentModel> result = await getDataWithScreenStatus(
-        dataStatus: commentStatus,
-        getData: () async => await _noticeRepository.getCommentList(nid),
-        isInitial: true,
-        isList: true);
-
-    if (result != null) {
-      commentList.clear();
-      commentList.addAll(result);
-
-      checkEmptyWithScreenStatus(dataStatus: commentStatus, result: result);
-      if (commentStatus.value == Status.loaded)
-        replyStatus.value = Status.loaded;
-    }
   }
 }
