3d28a3945df95d712aac9dcb75c0a675d3257efe
Funncy <gywns41@naver.com>
2021-05-07 23:37:55 +0900

    Modify model structure

--- lib/models/notice.dart
+++ lib/models/notice.dart
@@ -8,16 +8,15 @@ class NoticeModel extends Core {
   final String content;
   final List<String> imageList = [];
   int commentCount;
+  List<String> favoriteUserList = [];
   final DocumentReference documentReference;
-  int favoriteCount;
-  List<FavoriteModel> favoriteList = [];
 
   NoticeModel({
     @required this.documentId,
     @required this.userId,
     @required this.content,
     @required this.commentCount,
-    @required this.favoriteCount,
+    @required this.favoriteUserList,
     @required this.documentReference,
   });
 
@@ -29,10 +28,10 @@ class NoticeModel extends Core {
       userId: json['user_id'],
       content: json['content'],
       commentCount: json['comment_count'],
-      favoriteCount: json['favorite_count'],
+      favoriteUserList: json['favorite_user_list'],
       documentReference: doc.reference,
     );
-    // print((json['createdAt'] as Timestamp).toDate().toString());
+
     model.createdAt = (json['createdAt'] as Timestamp)?.toDate() ?? null;
     model.updatedAt = (json['updatedAt'] as Timestamp)?.toDate() ?? null;
     return model;
@@ -44,7 +43,7 @@ class NoticeModel extends Core {
         'content': content,
         'images': imageList,
         'comment_count': commentCount,
-        'favorite_count': favoriteCount,
+        'favorite_user_list': favoriteUserList,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
@@ -53,30 +52,27 @@ class NoticeModel extends Core {
         'user_id': userId,
         'content': content,
         'comment_count': commentCount,
-        'favorite_count': favoriteCount,
+        'favorite_user_list': favoriteUserList,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
 }
 
+//Inner Collection
 class NoticeCommentModel extends Core {
   final String docuemtnId;
-  //Notice ID
-  final String noticeId;
   final String userId;
   final String content;
-  int replyCount;
+  List<Map<String, Object>> replyList;
+  List<String> favoriteUserList;
   final DocumentReference documentReference;
-  int favoriteCount;
-  List<NoticeCommentReplyModel> replyList;
 
   NoticeCommentModel(
       {@required this.docuemtnId,
-      @required this.noticeId,
       @required this.userId,
       @required this.content,
-      @required this.replyCount,
-      @required this.favoriteCount,
+      @required this.replyList,
+      @required this.favoriteUserList,
       @required this.documentReference});
 
   factory NoticeCommentModel.fromFirestore(DocumentSnapshot doc) {
@@ -84,11 +80,10 @@ class NoticeCommentModel extends Core {
 
     var model = NoticeCommentModel(
       docuemtnId: doc.id,
-      noticeId: json['notice_id'],
       userId: json['user_id'],
       content: json['content'],
-      replyCount: json['reply_count'],
-      favoriteCount: json['favorite_count'],
+      replyList: json['reply_list'],
+      favoriteUserList: json['favorite_user_list'],
       documentReference: doc.reference,
     );
 
@@ -99,61 +94,49 @@ class NoticeCommentModel extends Core {
 
   Map<String, dynamic> toJson() => {
         'document_id': docuemtnId,
-        'notice_id': noticeId,
         'user_id': userId,
         'content': content,
-        'reply_count': replyCount,
-        'favorite_count': favoriteCount,
+        'reply_list': replyList,
+        'favorite_user_list': favoriteUserList,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
 
   Map<String, dynamic> toSaveJson() => {
-        'notice_id': noticeId,
         'user_id': userId,
         'content': content,
-        'reply_count': replyCount,
-        'favorite_count': favoriteCount,
+        'reply_list': replyList,
+        'favorite_user_list': favoriteUserList,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
 }
 
 class NoticeCommentReplyModel extends Core {
-  final String documentId;
   final String userId;
   final String content;
-  int favoriteCount;
-  final DocumentReference documentReference;
+  List<String> favoriteUserList;
 
   NoticeCommentReplyModel(
-      {@required this.documentId,
-      @required this.userId,
+      {@required this.userId,
       @required this.content,
-      @required this.favoriteCount,
-      @required this.documentReference});
+      @required this.favoriteUserList});
 
-  factory NoticeCommentReplyModel.fromFirestore(DocumentSnapshot doc) {
-    var json = doc.data();
-
+  factory NoticeCommentReplyModel.fromJson(Map<String, Object> json) {
     var model = NoticeCommentReplyModel(
-      documentId: doc.id,
       userId: json['user_id'],
       content: json['content'],
-      favoriteCount: json['favorite_count'],
-      documentReference: doc.reference,
+      favoriteUserList: json['favorite_user_list'],
     );
-
     model.createdAt = (json['createdAt'] as Timestamp)?.toDate() ?? null;
     model.updatedAt = (json['updatedAt'] as Timestamp)?.toDate() ?? null;
     return model;
   }
 
   Map<String, dynamic> toJson() => {
-        'document_id': documentId,
         'user_id': userId,
         'content': content,
-        'favorite_count': favoriteCount,
+        'favorite_user_list': favoriteUserList,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
@@ -161,40 +144,7 @@ class NoticeCommentReplyModel extends Core {
   Map<String, dynamic> toSaveJson() => {
         'user_id': userId,
         'content': content,
-        'favorite_count': favoriteCount,
-        'createdAt': createdAt,
-        'updatedAt': updatedAt,
-      };
-}
-
-class FavoriteModel extends Core {
-  final String documentId;
-  final String userId;
-
-  FavoriteModel({@required this.documentId, @required this.userId});
-
-  factory FavoriteModel.fromFirestroe(DocumentSnapshot doc) {
-    var json = doc.data();
-
-    var model = FavoriteModel(
-      documentId: doc.id,
-      userId: json['user_id'],
-    );
-
-    model.createdAt = (json['createdAt'] as Timestamp)?.toDate() ?? null;
-    model.updatedAt = (json['updatedAt'] as Timestamp)?.toDate() ?? null;
-    return model;
-  }
-
-  Map<String, dynamic> toJson() => {
-        'document_id': documentId,
-        'user_id': userId,
-        'createdAt': createdAt,
-        'updatedAt': updatedAt,
-      };
-
-  Map<String, dynamic> toSaveJson() => {
-        'user_id': userId,
+        'favorite_user_list': favoriteUserList,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
