07edfbd91c866672d8a9beea70f61c06e30df2c0
Funncy <gywns41@naver.com>
2021-05-23 04:15:14 +0900

    Fix refresh bug when change tbodyWidget by tab bottomNav menu

--- lib/pages/bottom_navigation/main_screen.dart
+++ lib/pages/bottom_navigation/main_screen.dart
@@ -13,33 +13,33 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Constants.backgroundColor,
-        selectedItemColor: Constants.iconSelectedColor,
-        unselectedItemColor: Constants.iconColor,
-        currentIndex: _selectedIndex,
-        onTap: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        //TODO: 다른 메뉴들 넣어야함.
-        items: [
-          BottomNavigationBarItem(label: "공지사항", icon: Icon(Icons.favorite)),
-          BottomNavigationBarItem(label: "공지사항2", icon: Icon(Icons.favorite)),
-          BottomNavigationBarItem(label: "공지사항3", icon: Icon(Icons.favorite)),
-          BottomNavigationBarItem(label: "공지사항4", icon: Icon(Icons.favorite)),
-        ],
-      ),
-      body: Center(
-        child: _bodyWidgetList.elementAt(_selectedIndex),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Constants.backgroundColor,
+          selectedItemColor: Constants.iconSelectedColor,
+          unselectedItemColor: Constants.iconColor,
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          //TODO: 다른 메뉴들 넣어야함.
+          items: [
+            BottomNavigationBarItem(label: "공지사항", icon: Icon(Icons.favorite)),
+            BottomNavigationBarItem(label: "공지사항2", icon: Icon(Icons.favorite)),
+            BottomNavigationBarItem(label: "공지사항3", icon: Icon(Icons.favorite)),
+            BottomNavigationBarItem(label: "공지사항4", icon: Icon(Icons.favorite)),
+          ],
+        ),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _bodyWidgetList,
+        ));
   }
 
   //바텀 네비게이션바에 연결된 BodyWidget들
-  List _bodyWidgetList = [
+  List<Widget> _bodyWidgetList = [
     NoticeBodyScreen(),
     Text("공지사항2"),
     Text("공지사항3"),
