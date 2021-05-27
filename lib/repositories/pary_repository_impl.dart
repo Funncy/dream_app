5c11be53b1e8c47c0be71bb929baa0f02ab21aa0
Funncy <gywns41@naver.com>
2021-05-27 23:14:47 +0900

    Implement pray_repository

---
+++ lib/repositories/pary_repository_impl.dart
@@ -0,0 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dream/core/error/error_model.dart';
+import 'package:dream/models/pray.dart';
+import 'package:dream/repositories/pray_repository.dart';
+import 'package:flutter/foundation.dart';
+
+class PrayRepositoryImpl extends PrayRepository {
+  FirebaseFirestore _firebaseFirestore;
+
+  PrayRepositoryImpl({@required FirebaseFirestore firebaseFirestore}) {
+    _firebaseFirestore = firebaseFirestore;
+  }
+
+  @override
+  Future<Either<ErrorModel, List<PrayModel>>> initPublicPrayList() async {
+    try {
+      var querySnapshot = await _firebaseFirestore
+          .collection('public_pray')
+          .orderBy('updated_at')
+          .limit(5)
+          .get();
+      List<PrayModel> result =
+          querySnapshot.docs.map((e) => PrayModel.fromFirestore(e)).toList();
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorModel(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<ErrorModel, List<PrayModel>>> addPublicPrayList(
+      DocumentReference documentReference) async {
+    try {
+      DocumentSnapshot documentSnapshot = await documentReference.get();
+      var querySnapshot = await _firebaseFirestore
+          .collection('public_pray')
+          .orderBy('updated_at')
+          .startAfterDocument(documentSnapshot)
+          .limit(5)
+          .get();
+      List<PrayModel> result =
+          querySnapshot.docs.map((e) => PrayModel.fromFirestore(e)).toList();
+      return Right(result);
+    } catch (e) {
+      return Left(ErrorModel(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<ErrorModel, void>> sendPray(
+      String userId, String title, String content, bool isPublic) async {
+    try {
+      var model = PrayModel(userId: userId, title: title, content: content);
+      CollectionReference collectionReference;
+      if (isPublic) {
+        collectionReference = _firebaseFirestore.collection('public_pray');
+      } else {
+        collectionReference = _firebaseFirestore.collection('private_pray');
+      }
+      collectionReference.add(model.toJson());
+      return Right(null);
+    } catch (e) {
+      return Left(ErrorModel(message: e.toString()));
+    }
+  }
+}
---
+++ lib/repositories/pray_repository.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dream/core/error/error_model.dart';
+import 'package:dream/models/pray.dart';
+
+abstract class PrayRepository {
+  //기도 페이지
+  //1. 기도 보내기
+  //3. 공개 기도 목록 가져오기
+
+  Future<Either<ErrorModel, void>> sendPray(
+      String userId, String title, String content, bool isPublic);
+  Future<Either<ErrorModel, List<PrayModel>>> initPublicPrayList();
+  Future<Either<ErrorModel, List<PrayModel>>> addPublicPrayList(
+      DocumentReference documentReference);
+}
