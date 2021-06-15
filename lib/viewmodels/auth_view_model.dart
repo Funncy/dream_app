ff066a85330e106e88b95ee627884c4e9e218bd0
Funncy <gywns41@naver.com>
2021-06-15 11:17:36 +0900

    Add status getter & setter

--- lib/viewmodels/auth_view_model.dart
+++ lib/viewmodels/auth_view_model.dart
@@ -1,6 +1,12 @@
+import 'package:dream/core/data_status/status_enum.dart';
 import 'package:dream/core/data_status/viewmodel_result.dart';
 
 abstract class AuthViewModel {
+  Status? get signInStatus;
+  set signInStatus(Status? status);
+  Status? get signUpStatus;
+  set signUpStatus(Status? status);
+
   Future<ViewModelResult> signInWithEmail(
       {required String email, required String password});
   Future<ViewModelResult> signUpWithEmail(
