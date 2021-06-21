42949ae090cfbbc58803cab088447c11c63659a3
Funncy <gywns41@naver.com>
2021-06-21 22:57:07 +0900

    Refactoring state from enum to Object

--- lib/app/core/state/view_state.dart
+++ lib/app/core/state/view_state.dart
@@ -1,1 +1,16 @@
-enum ViewState { initial, loading, loaded, error }
+// enum ViewState { initial, loading, loaded, error }
+
+import 'package:dream/app/core/error/error_model.dart';
+
+abstract class ViewState {}
+
+class Initial extends ViewState {}
+
+class Loading extends ViewState {}
+
+class Loaded extends ViewState {}
+
+class Error extends ViewState {
+  ErrorModel errorModel;
+  Error(this.errorModel);
+}
