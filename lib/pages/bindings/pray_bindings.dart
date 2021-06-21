77b55ce130d74dc864a8c65720cefb6b8a8a567f
Funncy <gywns41@naver.com>
2021-06-21 21:07:00 +0900

    Applied modifed viewmodel in bindings

--- lib/pages/bindings/pray_bindings.dart
+++ lib/pages/bindings/pray_bindings.dart
@@ -1,12 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dream/repositories/pary_repository_impl.dart';
-import 'package:dream/viewmodels/pray_view_model.dart';
+import 'package:dream/viewmodels/pray_list_view_model.dart';
+import 'package:dream/viewmodels/pray_send_view_model.dart';
 import 'package:get/get.dart';
 
 class PrayBindings implements Bindings {
   @override
   void dependencies() {
-    Get.put<PrayViewModel>(PrayViewModel(
+    Get.put<PrayListViewModel>(PrayListViewModel(
+        prayRepository: PrayRepositoryImpl(
+      firebaseFirestore: FirebaseFirestore.instance,
+    )));
+    Get.put<PraySendViewModel>(PraySendViewModel(
         prayRepository: PrayRepositoryImpl(
       firebaseFirestore: FirebaseFirestore.instance,
     )));
