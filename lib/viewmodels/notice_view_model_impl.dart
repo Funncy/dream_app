ae31f02b32a631d7d0bdb6b9abe007bedbd9ff3f
Funncy <gywns41@naver.com>
2021-06-21 16:04:03 +0900

    Refactoring notice_view_model

--- lib/viewmodels/notice_view_model_impl.dart
+++ lib/viewmodels/notice_view_model_impl.dart
@@ -1,28 +1,24 @@
 import 'package:dartz/dartz.dart';
 import 'package:dream/core/data_status/data_result.dart';
-import 'package:dream/core/data_status/status_enum.dart';
-import 'package:dream/core/data_status/viewmodel_result.dart';
+import 'package:dream/core/data_status/view_state.dart';
 import 'package:dream/core/error/default_error_model.dart';
 import 'package:dream/core/error/error_model.dart';
 import 'package:dream/models/notice.dart';
 import 'package:dream/repositories/notice_repository.dart';
-import 'package:dream/viewmodels/mixin/view_model_pipe_line_mixin.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-import 'notice_view_model.dart';
-
-class NoticeViewModelImpl extends GetxController
-    with ViewModelPipeLineMixin
-    implements NoticeViewModel {
+class NoticeViewModelImpl extends GetxController {
   late NoticeRepository _noticeRepository;
   //obs는 observer로 Rx로 데이터 관리되게 됩니다.
   //화면 상태가 아니라 데이터의 상태를 관리하자.
   List<NoticeModel> _noticeList = <NoticeModel>[];
   List<NoticeModel> get noticeList => _noticeList;
-  Rxn<Status?> _noticeStatus = Rxn<Status?>(Status.initial);
-  Status? get noticeStatus => _noticeStatus.value;
-  set noticeStatus(Status? status) => _noticeStatus.value = status;
+  Rxn<ViewState?> _noticeStatus = Rxn<ViewState?>(ViewState.initial);
+  ViewState? get noticeStatus => _noticeStatus.value;
+  Stream<ViewState?> get noticeStatusStream => _noticeStatus.stream;
+  late ErrorModel _errorModel;
+  ErrorModel? get errorModel => _errorModel;
 
   NoticeViewModelImpl({required NoticeRepository noticeRepository}) {
     _noticeRepository = noticeRepository;
@@ -32,87 +28,65 @@ class NoticeViewModelImpl extends GetxController
     _noticeRepository.createDummyData();
   }
 
-  Future<ViewModelResult> getNoticeList() => process(functionList: [
-        (_) => _getNoticeList(),
-      ], status: _noticeStatus);
-
-  Future<ViewModelResult> getMoreNoticeList() => process(functionList: [
-        (_) => _getMoreNoticeList(),
-      ], status: _noticeStatus);
-
-  Future<ViewModelResult> toggleNoticeFavorite(
-          {required String? noticeId, required String userId}) =>
-      process(functionList: [
-        (_) => _toggleNoticeFavorite(noticeId: noticeId, userId: userId),
-      ], status: _noticeStatus);
-
-  void refreshNotice() {
-    _noticeStatus.refresh();
-  }
-
-  /*
-  *
-  *
-  * 함수 부품들
-  *
-  *    
-  */
-
-  Future<DataResult> _getNoticeList() async {
+  Future<void> getNoticeList() async {
+    _setState(ViewState.loading);
     Either<ErrorModel, List<NoticeModel>> either =
         await _noticeRepository.getNoticeList();
     var result = either.fold((l) => l, (r) => r);
 
     //에러인 경우 종료
     if (either.isLeft()) {
-      return DataResult(isCompleted: false, errorModel: result as ErrorModel);
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.error);
     }
 
     noticeList.clear();
     noticeList.addAll(result as List<NoticeModel>);
-
-    return DataResult(isCompleted: true);
+    _setState(ViewState.loaded);
   }
 
-  Future<DataResult> _getMoreNoticeList() async {
+  Future<void> getMoreNoticeList() async {
+    _setState(ViewState.loading);
     if (noticeList.length == 0) {
-      return DataResult(
-          isCompleted: false,
-          errorModel: DefaultErrorModel(code: 'noticeList item count is 0'));
+      _setErrorModel(code: 'noticeList item count is 0');
+      _setState(ViewState.loading);
     }
 
     Either<ErrorModel, List<NoticeModel>?> either = await _noticeRepository
         .getMoreNoticeList(noticeList.last.documentReference);
     var result = either.fold((l) => l, (r) => r);
-    //에러인 경우 종료
+
     if (either.isLeft()) {
-      return DataResult(isCompleted: false, errorModel: result as ErrorModel);
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.loading);
     }
 
     noticeList.addAll(result as List<NoticeModel>);
-    return DataResult(isCompleted: true);
+    _setState(ViewState.loaded);
   }
 
-  Future<DataResult> _toggleNoticeFavorite(
+  Future<void> toggleNoticeFavorite(
       {required String? noticeId, required String userId}) async {
-    //Notice의 좋아요 리스트 가져오기
-    DataResult successOrError = _getNoticeById(noticeId!);
-    if (!successOrError.isCompleted) {
-      return successOrError;
+    _setState(ViewState.loading);
+//Notice의 좋아요 리스트 가져오기
+    NoticeModel? noticeModel = _getNoticeById(noticeId!);
+    if (noticeModel == null) {
+      _setErrorModel(code: 'noticeList item count is 0');
+      _setState(ViewState.error);
     }
-    NoticeModel notice = successOrError.data['notice'];
     //이미 등록되있다면 삭제
-    successOrError = _isExistFavoriteUser(notice, userId);
-    if (!successOrError.isCompleted) {
-      return successOrError;
+    bool? isExist = _isExistFavoriteUser(noticeModel!, userId);
+    if (isExist == null) {
+      _setErrorModel(code: '_isExistFavoriteUser error');
+      _setState(ViewState.error);
     }
-    bool isExist = successOrError.data['isExist'];
 
     var either = await _noticeRepository.toggleNoticeFavorite(
-        noticeId: noticeId, userId: userId, isDelete: isExist);
+        noticeId: noticeId, userId: userId, isDelete: isExist!);
     var result = either.fold((l) => l, (r) => r);
     if (either.isLeft()) {
-      return DataResult(isCompleted: false, errorModel: result as ErrorModel);
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.error);
     }
 
     //local에서도 증가
@@ -120,34 +94,42 @@ class NoticeViewModelImpl extends GetxController
       notice.favoriteUserList!.remove(userId);
     else
       notice.favoriteUserList!.add(userId);
-    refreshNotice();
-    return DataResult(isCompleted: true);
+    _setState(ViewState.loaded);
   }
 
-  DataResult _getNoticeById(String noticeId) {
+  void refreshNotice() {
+    _noticeStatus.refresh();
+  }
+
+  NoticeModel? _getNoticeById(String noticeId) {
     late NoticeModel notice;
     try {
       notice = noticeList.where((notice) => notice.id == noticeId).first;
+      return notice;
     } catch (e) {
-      return DataResult(
-          isCompleted: false,
-          errorModel:
-              DefaultErrorModel(code: 'notice not found at _getNoticeById'));
+      return null;
     }
-    return DataResult(isCompleted: true, data: {'notice': notice});
   }
 
-  DataResult _isExistFavoriteUser(NoticeModel notice, String userId) {
+  bool? _isExistFavoriteUser(NoticeModel notice, String userId) {
     late bool isExist;
     try {
       isExist = notice.favoriteUserList!
           .where((element) => element == userId)
           .isNotEmpty;
+      return isExist;
     } catch (e) {
-      return DataResult(
-          isCompleted: false,
-          errorModel: DefaultErrorModel(code: 'error at _isExistFavoriteUser'));
+      return null;
     }
-    return DataResult(isCompleted: true, data: {'isExist': isExist});
+  }
+
+  _setState(ViewState state) {
+    _noticeStatus.value = state;
+  }
+
+  _setErrorModel({ErrorModel? errorModel, String? code}) {
+    if (errorModel != null)
+      _errorModel = errorModel;
+    else if (code != null) _errorModel = DefaultErrorModel(code: code!);
   }
 }
