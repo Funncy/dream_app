bbbe16687d560ed6323c6b340c1c6117f0bf4e2b
Funncy <gywns41@naver.com>
2021-06-21 17:52:23 +0900

    Refactoring auth_view_model

--- lib/viewmodels/auth_view_model.dart
+++ lib/viewmodels/auth_view_model.dart
@@ -1,21 +1,139 @@
 import 'dart:io';
 
-import 'package:dream/core/data_status/status_enum.dart';
-import 'package:dream/core/data_status/viewmodel_result.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dream/core/data_status/fireauth_status.dart';
+import 'package:dream/core/data_status/view_state.dart';
+import 'package:dream/core/error/default_error_model.dart';
+import 'package:dream/core/error/error_model.dart';
+import 'package:dream/models/user.dart';
+import 'package:dream/repositories/auth_repository.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
-abstract class AuthViewModel {
-  Status? get signInStatus;
-  set signInStatus(Status? status);
-  Status? get signUpStatus;
-  set signUpStatus(Status? status);
+class AuthViewModel extends GetxController {
+  late AuthRepository _authRepository;
 
-  Future<ViewModelResult> signInWithEmail(
-      {required String email, required String password});
-  Future<ViewModelResult> signUpWithEmail(
+  Rxn<ViewState?> _authStatus = Rxn<ViewState?>(ViewState.initial);
+
+  Rxn<FireAuthStatus?> _fireauthStatus =
+      Rxn<FireAuthStatus?>(FireAuthStatus.loading);
+  FireAuthStatus? get fireauthStatus => _fireauthStatus.value;
+  ViewState? get authStatus => _authStatus.value;
+  set authStatus(ViewState? status) => _authStatus.value = status;
+  Stream<ViewState?> get authStatusStream => _authStatus.stream;
+
+  Rxn<UserModel> _user = Rxn<UserModel>();
+  // Rxn<User> _firebaseUser = Rxn<User>();
+  UserModel? get user => _user.value;
+
+  late ErrorModel _errorModel;
+  ErrorModel? get errorModel => _errorModel;
+
+  AuthViewModel({required authRepository}) {
+    _authRepository = authRepository;
+  }
+
+  @override
+  void onInit() {
+    _authRepository.getAuthStateChanges().listen((user) {
+      if (_user.value == null && user == null)
+        _changeFireAuthStatus(status: FireAuthStatus.signout);
+      if (_user.value != user) {
+        _user.value = user;
+        _changeFireAuthStatus();
+      }
+    });
+    super.onInit();
+  }
+
+  void _changeFireAuthStatus({FireAuthStatus? status}) {
+    if (status != null) {
+      _fireauthStatus.value = status;
+    } else {
+      if (_user.value != null)
+        _fireauthStatus.value = FireAuthStatus.signin;
+      else
+        _fireauthStatus.value = FireAuthStatus.signout;
+    }
+  }
+
+  Future<void> signInWithEmail(
+      {required String email, required String password}) async {
+    _setState(ViewState.loading);
+
+    Either<ErrorModel, UserModel> either =
+        await _authRepository.signInWithEmail(email, password);
+    var result = either.fold((l) => l, (r) => r);
+    if (either.isLeft()) {
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.error);
+      return;
+    }
+
+    _user.value = result as UserModel;
+    _setState(ViewState.loaded);
+  }
+
+  Future<void> signUpWithEmail(
       {required String email,
       required String password,
       required String name,
-      required String group});
-  Future<ViewModelResult> signOut();
-  Future<ViewModelResult> setProfileImage({required File imageFile});
+      required String group}) async {
+    _setState(ViewState.loading);
+    Either<ErrorModel, UserModel> either =
+        await _authRepository.signUpWithEmail(
+            email: email, password: password, name: name, group: group);
+    var result = either.fold((l) => l, (r) => r);
+    if (either.isLeft()) {
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.error);
+      return;
+    }
+
+    _user.value = result as UserModel;
+    _setState(ViewState.loaded);
+  }
+
+  Future<void> signOut() async {
+    _setState(ViewState.loading);
+    Either<ErrorModel, void> either = await _authRepository.signOut();
+    var result = either.fold((l) => l, (r) => r);
+    if (either.isLeft()) {
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.error);
+      return;
+    }
+    _setState(ViewState.loaded);
+  }
+
+  Future<void> setProfileImage({required File imageFile}) async {
+    _setState(ViewState.loading);
+    late String imageUrl;
+
+    Either<ErrorModel, String> either =
+        await _authRepository.setProfileImage(uid: user!.id, image: imageFile);
+    var result = either.fold((l) => l, (r) => r);
+    if (either.isLeft()) {
+      _setErrorModel(errorModel: result as ErrorModel);
+      _setState(ViewState.error);
+      return;
+    }
+
+    imageUrl = result as String;
+
+    _user.update((user) {
+      user!.profileImageUrl = imageUrl;
+    });
+    _setState(ViewState.loaded);
+  }
+
+  _setState(ViewState state) {
+    _authStatus.value = state;
+  }
+
+  _setErrorModel({ErrorModel? errorModel, String? code}) {
+    if (errorModel != null)
+      _errorModel = errorModel;
+    else if (code != null) _errorModel = DefaultErrorModel(code: code);
+  }
 }
