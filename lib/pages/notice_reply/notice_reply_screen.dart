75cd16839e49c0cee4102bf7f649b54164568936
Funncy <gywns41@naver.com>
2021-06-21 21:04:01 +0900

    Applied modified viewmodel in notice_reply

--- lib/pages/notice_reply/notice_reply_screen.dart
+++ lib/pages/notice_reply/notice_reply_screen.dart
@@ -1,16 +1,17 @@
 import 'dart:async';
 
-import 'package:dream/core/data_status/view_state.dart';
-import 'package:dream/models/comment.dart';
-import 'package:dream/models/reply.dart';
+import 'package:dream/app/core/state/view_state.dart';
+import 'package:dream/app/data/models/comment.dart';
+import 'package:dream/app/data/models/reply.dart';
 import 'package:dream/pages/common/empty_widget.dart';
 import 'package:dream/pages/common/error_message_widget.dart';
 import 'package:dream/pages/common/loading_widget.dart';
-import 'package:dream/pages/common/screen_status_widget.dart';
 import 'package:dream/pages/common/alert_mixin.dart';
+import 'package:dream/pages/common/view_model_builder.dart';
 import 'package:dream/pages/notice_detail/components/bottom_input_bar.dart';
 import 'package:dream/pages/notice_detail/components/notice_comment.dart';
-import 'package:dream/viewmodels/comment_reply_view_model.dart';
+import 'package:dream/viewmodels/comment_view_model.dart';
+import 'package:dream/viewmodels/reply_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -26,8 +27,8 @@ class NoticeReplyScreen extends StatefulWidget {
 }
 
 class _NoticeReplyScreenState extends State<NoticeReplyScreen> with AlertMixin {
-  CommentReplyViewModel commentReplyViewModel =
-      Get.find<CommentReplyViewModel>();
+  ReplyViewModel replyViewModel = Get.find<ReplyViewModel>();
+  CommentViewModel commentViewModel = Get.find<CommentViewModel>();
   final TextEditingController _textEditingController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
   late StreamSubscription alertSubscription;
@@ -36,15 +37,17 @@ class _NoticeReplyScreenState extends State<NoticeReplyScreen> with AlertMixin {
     super.initState();
     //id에 해당하는 댓글 존재 확인
     WidgetsBinding.instance!.addPostFrameCallback((_) {
-      commentReplyViewModel.isExistCommentById(commentId: widget.commentId);
-      // alertSubscription = commentReplyViewModel.alert.listen((alertModel) {
-      //   if (alertModel.isAlert) return;
-      //   alertModel.isAlert = true;
-      //   showAlert(title: alertModel.title, content: alertModel.content);
-      // });
+      // replyViewModel.isExistCommentById(commentId: widget.commentId);
+
+      replyViewModel.replyStateStream.listen((state) {
+        if (state == ViewState.error || replyViewModel.errorModel != null) {
+          alertWithErrorModel(replyViewModel.errorModel);
+        }
+      });
+
       //새로운 답글 추가시 아래 스크롤 애니메이션
-      debounce(commentReplyViewModel.replyStatusStream, (dynamic _) {
-        if (commentReplyViewModel.replyStatus == ViewState.loaded) {
+      debounce(replyViewModel.rxReplyState, (_) {
+        if (replyViewModel.replyState == ViewState.loaded) {
           _scrollController.animateTo(
               _scrollController.position.maxScrollExtent,
               duration: Duration(milliseconds: 500),
@@ -62,14 +65,14 @@ class _NoticeReplyScreenState extends State<NoticeReplyScreen> with AlertMixin {
 
   void writeReply() async {
     //TODO: uid 실제 유저로 바꿔야함.
-    await commentReplyViewModel.writeReply(
+    await replyViewModel.writeReply(
         noticeId: widget.noticeId,
         commentId: widget.commentId,
         userId: '123',
         content: _textEditingController.text);
     _textEditingController.text = '';
     //뒤로 갈 경우 화면 적용시키기 위함
-    commentReplyViewModel.refreshComment();
+    commentViewModel.refresh();
   }
 
   void deleteReply(String commentId, ReplyModel replyModel) {
@@ -78,11 +81,11 @@ class _NoticeReplyScreenState extends State<NoticeReplyScreen> with AlertMixin {
         content: '정말 답글을 삭제하시겠습니까?',
         isFunction: true,
         function: () async {
-          await commentReplyViewModel.deleteReply(
+          await replyViewModel.deleteReply(
               noticeId: widget.noticeId,
               commentId: commentId,
               replyModel: replyModel);
-          commentReplyViewModel.refreshComment();
+          commentViewModel.refresh();
         });
   }
 
@@ -101,43 +104,44 @@ class _NoticeReplyScreenState extends State<NoticeReplyScreen> with AlertMixin {
         backgroundColor: Colors.white,
       ),
       body: SafeArea(
-        child: Obx(() {
-          ViewState dataStatus = commentReplyViewModel.replyStatus!;
+        child: Container(
+          child: Stack(children: [
+            Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    controller: _scrollController,
+                    child: ViewModelBuilder(
+                        init: replyViewModel.isEixstReply(
+                            commentId: widget.commentId),
+                        errorWidget: _errorWidget(),
+                        loadingWidget: _loadingWidget(),
+                        getState: () => commentViewModel.commentState,
+                        builder: (context, snapshot) {
+                          return Obx(() {
+                            var dataStatus = replyViewModel.replyState;
+                            var comment = commentViewModel.commentList
+                                .firstWhere((e) => e!.id == widget.commentId);
 
-          return Container(
-            child: Stack(children: [
-              Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      controller: _scrollController,
-                      child: Obx(() {
-                        var dataStatus = commentReplyViewModel.replyStatus;
-                        var comment = commentReplyViewModel.commentList
-                            .firstWhere((e) => e!.id == widget.commentId);
-                        return DataStatusWidget(
-                            body: _commentBody(comment),
-                            error: _errorWidget(),
-                            loading: _loadingWidget(),
-                            empty: _emptyWidget(),
-                            updating: _updatingWidget(comment),
-                            dataStatus: dataStatus!);
-                      }),
-                    ),
+                            return Stack(
+                              children: [
+                                _commentBody(comment),
+                                if (dataStatus == ViewState.loading)
+                                  _loadingWidget(),
+                              ],
+                            );
+                          });
+                        }),
                   ),
-                  BottonInputBar(
-                    textEditingController: _textEditingController,
-                    writeFunction: writeReply,
-                  ),
-                ],
-              ),
-              if (dataStatus == ViewState.updating)
-                Center(
-                  child: CircularProgressIndicator(),
-                )
-            ]),
-          );
-        }),
+                ),
+                BottonInputBar(
+                  textEditingController: _textEditingController,
+                  writeFunction: writeReply,
+                ),
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
@@ -145,21 +149,6 @@ class _NoticeReplyScreenState extends State<NoticeReplyScreen> with AlertMixin {
   ErrorMessageWidget _errorWidget() => ErrorMessageWidget(errorMessage: 'test');
 
   EmptyWidget _emptyWidget() => EmptyWidget();
-
-  Stack _updatingWidget(CommentModel? commentModel) {
-    return Stack(
-      children: [
-        NoticeComment(
-          noticeCommentModel: commentModel,
-          isReplyScreen: true,
-        ),
-        Container(
-          height: 400.h,
-          child: Center(child: CircularProgressIndicator()),
-        )
-      ],
-    );
-  }
 
   Padding _loadingWidget() {
     return Padding(
