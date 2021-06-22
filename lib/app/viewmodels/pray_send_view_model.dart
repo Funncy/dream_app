7fcfdfc848f46be2c81051f1d9882409d9481410
Funncy <gywns41@naver.com>
2021-06-22 10:45:34 +0900

    Applied modified state object in pray_send_view_model

--- lib/app/viewmodels/pray_send_view_model.dart
+++ lib/app/viewmodels/pray_send_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:dream/app/core/error/default_error_model.dart';
-import 'package:dream/app/core/error/error_model.dart';
+import 'package:dream/app/core/error/failure.dart';
 import 'package:dream/app/core/state/view_state.dart';
 import 'package:dream/app/repositories/pray_repository.dart';
 import 'package:get/get.dart';
@@ -8,12 +7,9 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 
 class PraySendViewModel extends GetxController {
   late PrayRepository _prayRepository;
-  Rx<ViewState> _sendState = ViewState.initial.obs;
-  ViewState get sendState => _sendState.value;
+  Rxn<ViewState> _sendState = Rxn(Initial());
+  ViewState? get sendState => _sendState.value;
   Stream<ViewState?> get sendStateStream => _sendState.stream;
-
-  ErrorModel? _errorModel;
-  ErrorModel? get errorModel => _errorModel;
 
   PraySendViewModel({required PrayRepository prayRepository}) {
     _prayRepository = prayRepository;
@@ -24,16 +20,15 @@ class PraySendViewModel extends GetxController {
       required String title,
       required String content,
       required bool? isPublic}) async {
-    _setState(ViewState.loading);
-    Either<ErrorModel, void> either =
+    _setState(Loading());
+    Either<Failure, void> either =
         await _prayRepository.sendPray(userId, title, content, isPublic);
     either.fold((l) {
-      _setErrorModel(errorModel: l);
-      _setState(ViewState.error);
+      _setState(Error(l));
     }, (r) => r);
     if (either.isLeft()) return;
 
-    _setState(ViewState.loaded);
+    _setState(Loaded());
   }
 
   refresh() {
@@ -42,11 +37,5 @@ class PraySendViewModel extends GetxController {
 
   _setState(ViewState state) {
     _sendState.value = state;
-  }
-
-  _setErrorModel({ErrorModel? errorModel, String? code}) {
-    if (errorModel != null)
-      _errorModel = errorModel;
-    else if (code != null) _errorModel = DefaultErrorModel(code: code);
   }
 }
