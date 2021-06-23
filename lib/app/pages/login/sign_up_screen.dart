76d4e25874dba7d4caa12ac7febf670e0ea0aaca
Funncy <gywns41@naver.com>
2021-06-23 21:25:03 +0900

    Add loading widget in sign_up_screen

--- lib/app/pages/login/sign_up_screen.dart
+++ lib/app/pages/login/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:dream/app/core/state/view_state.dart';
 import 'package:dream/app/pages/common/alert_mixin.dart';
 import 'package:dream/app/pages/common/input_mixin.dart';
+import 'package:dream/app/pages/common/loading_widget.dart';
 import 'package:dream/app/viewmodels/auth_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -70,99 +71,112 @@ class _SignUpScreenState extends State<SignUpScreen>
       appBar: AppBar(
         title: Text("회원 가입"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(Constants.commonGap),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              SizedBox(height: Constants.commonLGap),
-              TextFormField(
-                controller: _emailController,
-                cursorColor: Colors.black54,
-                decoration: textInputDecor('이메일'),
-                validator: (text) {
-                  if (text!.isNotEmpty && text.contains('@')) {
-                    return null;
-                  } else {
-                    return '정확한 이메일 주소를 입력해주세요.';
-                  }
-                },
-              ),
-              SizedBox(height: Constants.commonLGap),
-              TextFormField(
-                controller: _pwController,
-                cursorColor: Colors.black54,
-                obscureText: true,
-                decoration: textInputDecor('비밀번호'),
-                validator: (text) {
-                  if (text!.isNotEmpty && text.length > 4) {
-                    return null;
-                  } else {
-                    return '비밀번호를 4자리 이상 입력해주세요.';
-                  }
-                },
-              ),
-              SizedBox(height: Constants.commonLGap),
-              TextFormField(
-                controller: _cpwController,
-                cursorColor: Colors.black54,
-                obscureText: true,
-                decoration: textInputDecor('비밀번호 확인'),
-                validator: (text) {
-                  if (text!.isNotEmpty && _pwController.text == text) {
-                    return null;
-                  } else {
-                    return '입력값이 비밀번호와 일치하지 않습니다.';
-                  }
-                },
-              ),
-              Divider(),
-              TextFormField(
-                controller: _nameController,
-                cursorColor: Colors.black54,
-                decoration: textInputDecor('이름'),
-                validator: (text) {
-                  if (text!.isNotEmpty) {
-                    return null;
-                  } else {
-                    return '이름을 입력해주세요';
-                  }
-                },
-              ),
-              SizedBox(height: Constants.commonLGap),
-              InputDecorator(
-                decoration: textInputDecor('지파를 선택해주세요.'),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    value: _groupName,
-                    isDense: true,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _groupName = newValue!;
-                      });
-                    },
-                    items: _groupNames.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
+      body: Obx(() {
+        var authState = _authViewModel.authState;
+        return Stack(
+          children: [
+            _signUpFormWidget(),
+            if (authState is Loading) _blackBodyWidget(),
+            if (authState is Loading) _loadingWidget(),
+          ],
+        );
+      }),
+    );
+  }
+
+  Padding _signUpFormWidget() {
+    return Padding(
+      padding: const EdgeInsets.all(Constants.commonGap),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: Constants.commonLGap),
+            TextFormField(
+              controller: _emailController,
+              cursorColor: Colors.black54,
+              decoration: textInputDecor('이메일'),
+              validator: (text) {
+                if (text!.isNotEmpty && text.contains('@')) {
+                  return null;
+                } else {
+                  return '정확한 이메일 주소를 입력해주세요.';
+                }
+              },
+            ),
+            SizedBox(height: Constants.commonLGap),
+            TextFormField(
+              controller: _pwController,
+              cursorColor: Colors.black54,
+              obscureText: true,
+              decoration: textInputDecor('비밀번호'),
+              validator: (text) {
+                if (text!.isNotEmpty && text.length > 4) {
+                  return null;
+                } else {
+                  return '비밀번호를 4자리 이상 입력해주세요.';
+                }
+              },
+            ),
+            SizedBox(height: Constants.commonLGap),
+            TextFormField(
+              controller: _cpwController,
+              cursorColor: Colors.black54,
+              obscureText: true,
+              decoration: textInputDecor('비밀번호 확인'),
+              validator: (text) {
+                if (text!.isNotEmpty && _pwController.text == text) {
+                  return null;
+                } else {
+                  return '입력값이 비밀번호와 일치하지 않습니다.';
+                }
+              },
+            ),
+            Divider(),
+            TextFormField(
+              controller: _nameController,
+              cursorColor: Colors.black54,
+              decoration: textInputDecor('이름'),
+              validator: (text) {
+                if (text!.isNotEmpty) {
+                  return null;
+                } else {
+                  return '이름을 입력해주세요';
+                }
+              },
+            ),
+            SizedBox(height: Constants.commonLGap),
+            InputDecorator(
+              decoration: textInputDecor('지파를 선택해주세요.'),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: _groupName,
+                  isDense: true,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _groupName = newValue!;
+                    });
+                  },
+                  items: _groupNames.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
               ),
-              SizedBox(height: Constants.commonLGap),
-              _submitBtn(context, _formKey),
-              Divider(),
-              SizedBox(height: Constants.commonLGap),
-            ],
-          ),
+            ),
+            SizedBox(height: Constants.commonLGap),
+            _submitBtn(_formKey),
+            Divider(),
+            SizedBox(height: Constants.commonLGap),
+          ],
         ),
       ),
     );
   }
 
-  TextButton _submitBtn(BuildContext context, GlobalKey<FormState> _formKey) {
+  TextButton _submitBtn(GlobalKey<FormState> _formKey) {
     FocusScopeNode currentFocus = FocusScope.of(context);
 
     return TextButton(
@@ -175,4 +189,12 @@ class _SignUpScreenState extends State<SignUpScreen>
       child: Text('회원가입', style: TextStyle(color: Colors.black)),
     );
   }
+
+  LoadingWidget _loadingWidget() => LoadingWidget();
+
+  Widget _blackBodyWidget() => Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        color: Colors.black38,
+      );
 }
